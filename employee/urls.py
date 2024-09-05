921acd733428c8bc40cdcead57332c1fd6b28b3a
mickfree <lydoived@gmail.com>
2024-09-05 16:24:35 -0500

    add templates list supervisor & technician, implements sidebar icons for redict templates and add icon employee

--- employee/urls.py
+++ employee/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('login/', views.Login.as_view(), name='login'),
     path("register/", views.RegisterView.as_view(), name="register"),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('list_supervisor/', views.supervisor_list_view, name="supervisor"),
+    path('list_technical/', views.technical_list_view, name="technical"),
 ]
