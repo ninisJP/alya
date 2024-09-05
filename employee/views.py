d64e57f2fb2d631ea9e4f5a1115a0149489566a7
pintornani <pintorlocal@proton.me>
2024-09-05 16:18:41 +0000

    Fix create follow/control/ create new cards
    -Add logout
    -Add create card when user loggin

--- employee/views.py
+++ employee/views.py
@@ -1,23 +1,26 @@
-from .models import *
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views.generic import FormView
+
 from employee.forms import RegisterForm
+
+from .models import *
+
+from follow_control_card.utils import create_monthly_cards_for_user
 
 
 class Login(LoginView):
     template_name = 'registration/login.html'
     def form_valid(self, form):
         response = super().form_valid(form)
-        
+        create_monthly_cards_for_user(self.request.user)
         return response
-    
-    
+
 class RegisterView(FormView):
     form_class = RegisterForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
     def form_valid(self, form):
-        user = form.save() 
+        user = form.save()
         return super().form_valid(form)
