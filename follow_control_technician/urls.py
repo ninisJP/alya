a7c58a183854f349f86caa62dff31e912b33d028
adsoog <iadso.ao@gmail.com>
2024-09-06 23:22:44 -0500

    Added a bunch of stuffs rip alya btw

--- follow_control_technician/urls.py
+++ follow_control_technician/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
 
-from .views import TechniciansMonth, create_technician, create_technician_card, create_technician_task, view_technician_card
+from .views import TechniciansMonth, create_technician_card, view_technician_card, add_task_form
 
 urlpatterns = [
-    # URL para el informe de tarjetas de técnicos del mes
     path('informe-tecnicos/<int:mes>/<int:anio>/', TechniciansMonth.as_view(), name='technicians_month'),
     path('ver-tarjeta-tecnico/<int:tecnico_id>/<int:dia>/<int:mes>/<int:anio>/', view_technician_card, name='view_technician_card'),
-    path('crear-tecnico/', create_technician, name='create_technician'),
-    path('crear-tarea-tecnico/', create_technician_task, name='create_technician_task'),
     path('crear-tarjeta-tecnico/', create_technician_card, name='create_technician_card'),
-    path('crear-tarjeta-tecnico/<int:tecnico_id>/<int:dia>/<int:mes>/<int:anio>/', create_technician_card, name='create_technician_card'),
+    path('add-task-form/', add_task_form, name='add_task_form'),
 ]
