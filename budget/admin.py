c7769f770cd3e2b71767e758e86a5e80ef2c0b5a
Adsoog <iadso.ao@gmail.com>
2024-09-16 16:12:58 -0500

    subido incompleto blanquita esta muriendo :(

--- budget/admin.py
+++ budget/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Budget, Item, BudgetItem
 
-# Register your models here.
+# Registrar el modelo Item
+admin.site.register(Item)
+
+# Registrar el modelo BudgetItem
+admin.site.register(BudgetItem)
+
+# Registrar el modelo Budget con opciones adicionales
+@admin.register(Budget)
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ('budget_name', 'budget_days', 'budget_date', 'budget_final_price')
+    search_fields = ('budget_name',)
+    list_filter = ('budget_date',)
