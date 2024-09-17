057bfb63761f7d6adb880479349fea7fa13e453d
pintornani <pintorlocal@proton.me>
2024-09-17 20:09:29 +0000

    Add budget/catalog
    -Add CatalogItem to model and views

--- budget/forms.py
+++ budget/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Budget, BudgetItem
+from .models import Budget, BudgetItem, CatalogItem
 from django.forms import inlineformset_factory
 
 class BudgetForm(forms.ModelForm):
@@ -8,8 +8,8 @@ class BudgetForm(forms.ModelForm):
         fields = ['client', 'budget_name', 'budget_days', 'budget_date']
         widgets = {
             'budget_date': forms.DateInput(attrs={
-                'type': 'date', 
-                'class': 'form-control', 
+                'type': 'date',
+                'class': 'form-control',
                 'style': 'max-width: 200px;'  # Ajusta el tamaño aquí
             }),
         }
@@ -27,3 +27,19 @@ BudgetItemFormSet = inlineformset_factory(
     extra=1,
     can_delete=True
 )
+
+class CatalogItemForm(forms.ModelForm):
+    class Meta:
+        model = CatalogItem
+        fields = ('name', 'description', 'category', 'price', 'price_per_day')
+        labels = {
+            'name': 'Nombre del Item',
+            'description': 'Descripcion',
+            'category': 'Categoria',
+            'price': 'Precio',
+            'price_per_day': 'Precio por dia',
+        }
+
+class SearchCatalogItemForm(forms.Form):
+    name = forms.CharField(label="name", max_length=100, required=False)
+    description = forms.CharField(label="description", max_length=100, required=False)
