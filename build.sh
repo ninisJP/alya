20e2221b298ff6f4cb79f5b50f075ec603709322
iAdso <iadso.ao@gmail.com>
2024-09-25 16:40:48 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,15 +2,6 @@
 # Exit on error
 set -o errexit
 
-# Instalar dependencias
-pip install -r requirements.txt
-
-# Recoger archivos estáticos
-python manage.py collectstatic --no-input
-
-# Aplicar migraciones de la base de datos
-python manage.py migrate
-
 # Crear un superusuario automáticamente
 echo "from django.contrib.auth import get_user_model; \
       User = get_user_model(); \
