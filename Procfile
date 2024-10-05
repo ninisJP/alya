b116bd608e35a9c8b67f9cc916255791e291dab6
adsoog <iadso.ao@gmail.com>
2024-10-04 23:50:08 -0500

    ultimate fix v2

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py migrate && python manage.py collectstatic --noinput && gunicorn alya.wsgi
+web: python manage.py makemigrations && python manage.py migrate && ./build.sh && python manage.py collectstatic && gunicorn alya.wsgi
--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
+
+# Crear un superusuario automáticamente
+echo "from django.contrib.auth import get_user_model; \
+      User = get_user_model(); \
+      User.objects.filter(username='admin').exists() or \
+      User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')" | python manage.py shell
