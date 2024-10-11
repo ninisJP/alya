845858bb5e6b384177e7dc0a2e0d404d5b949f54
adsoog <iadso.ao@gmail.com>
2024-10-10 20:58:36 -0500

    fixed validation erros

--- alya/settings.py
+++ alya/settings.py
@@ -90,16 +90,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'alya.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+# }
 
 
 
