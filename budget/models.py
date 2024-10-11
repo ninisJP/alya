3a1c74e4b58b0dd66d0d75ba6e16deb59cad0812
adsoog <iadso.ao@gmail.com>
2024-10-10 22:28:41 -0500

    ultimate fix main v8

--- budget/models.py
+++ budget/models.py
@@ -71,14 +71,15 @@ class CatalogItem(models.Model):
         MANODEOBRA = 'Mano de obra'
 
     category = models.CharField(max_length=100, choices=Category.choices, default=Category.EQUIPO)
-    description = models.CharField(max_length=256)
+    description = models.CharField(max_length=256, db_index=True)  # Índice añadido
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     price_per_day = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    sap = models.CharField(max_length=100, unique=True)
+    sap = models.CharField(max_length=100, unique=True, db_index=True)  # Índice añadido
     unit = models.CharField(max_length=100, default='UND')
 
     def __str__(self):
         return f'{self.sap} <{self.description}> precio: {self.price}'
+
 
 
 class BudgetItem(models.Model):
--- logistic_suppliers/models.py
+++ logistic_suppliers/models.py
@@ -4,7 +4,7 @@ from .utils import CURRENCY_CHOICES
 # Create your models here.
 class Suppliers(models.Model):
     document = models.CharField(max_length=20, unique=True, verbose_name="RUC/DNI", null=True, blank=True, default='')
-    name = models.CharField(max_length=100, verbose_name="Nombre del Proveedor", null=True, blank=True, default='')
+    name = models.CharField(max_length=100, verbose_name="Nombre del Proveedor", null=True, blank=True, default='', db_index=True)
     bank = models.CharField(max_length=100, verbose_name="Nombre del Banco", null=True, blank=True, default='')
     account = models.CharField(max_length=50, verbose_name="Número de Cuenta", null=True, blank=True, default='')
     currency = models.CharField(max_length=20,choices=CURRENCY_CHOICES, default="Soles", verbose_name="Moneda", null=True, blank=True)
@@ -16,3 +16,7 @@ class Suppliers(models.Model):
     class Meta:
         verbose_name = "Proveedor"
         verbose_name_plural = "Proveedores"
+        indexes = [
+            models.Index(fields=['name', 'document']),
+        ]
+
