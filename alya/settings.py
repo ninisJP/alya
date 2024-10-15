572cd64eef54a99dd413841344c026ab57c176c7
iAdso <iadso.ao@gmail.com>
2024-10-14 23:18:13 -0500

    Update settings.py

--- alya/settings.py
+++ alya/settings.py
@@ -91,12 +91,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'alya.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Motor de la base de datos
-        'NAME': BASE_DIR / 'db.sqlite3',         # Nombre y ubicación del archivo de la base de datos
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
