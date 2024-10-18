29e51edf775b4f297c4a64f12d799fcd60981860
iAdso <iadso.ao@gmail.com>
2024-10-18 15:16:45 +0000

    Update utils.py

--- accounting_order_sales/utils.py
+++ accounting_order_sales/utils.py
@@ -25,7 +25,8 @@ def procesar_archivo_excel(archivo_excel, salesorder_id):
     col_desc_articulo = 'Descripción del artículo'
     col_cantidad = 'Cantidad'
     col_precio_bruto = 'Precio bruto'
-    col_total_bruto = 'Total bruto (ML)'  # o 'Total (ML)', ajustar según sea necesario
+    col_total_bruto = 'Total bruto (ML)'
+    col_unidad_medida = 'Unidad de medida'  # Nueva columna para la unidad de medida
 
     # Encuentra los índices de las columnas
     header = [cell.value for cell in sheet[1]]
@@ -34,6 +35,7 @@ def procesar_archivo_excel(archivo_excel, salesorder_id):
     idx_cantidad = header.index(col_cantidad)
     idx_precio_bruto = header.index(col_precio_bruto)
     idx_total_bruto = header.index(col_total_bruto)
+    idx_unidad_medida = header.index(col_unidad_medida)  # Índice de la unidad de medida
 
     for row in sheet.iter_rows(min_row=2, values_only=True):
         if row[idx_nro_articulo] is None or row[idx_desc_articulo] is None or row[idx_cantidad] is None:
@@ -42,6 +44,7 @@ def procesar_archivo_excel(archivo_excel, salesorder_id):
         cantidad = limpiar_valor(row[idx_cantidad])
         precio_bruto = limpiar_valor(row[idx_precio_bruto])
         total_bruto = limpiar_valor(row[idx_total_bruto])
+        unidad_medida = row[idx_unidad_medida]  # Obtener unidad de medida directamente
 
         if cantidad is None:
             continue  # Si no se puede convertir la cantidad, saltar esta fila
@@ -53,4 +56,6 @@ def procesar_archivo_excel(archivo_excel, salesorder_id):
             amount=cantidad,
             price=precio_bruto,
             price_total=total_bruto,
+            unit_of_measurement=unidad_medida  # Guardar la unidad de medida
         )
+
