eb3b0158554fba3608efa8d4f3f89b60b7a2c351
pintornani <pintorlocal@proton.me>
2024-10-25 21:46:39 +0000

    create register

---
+++ logistic_inventory_register/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LogisticInventoryRegisterConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'logistic_inventory_register'
