7ffe81866951e0a6e78a53ccb961ed8e76383b0b
Adsoog <iadso.ao@gmail.com>
2024-11-11 04:57:13 +0000

    yololo

--- alya/settings.py
+++ alya/settings.py
@@ -97,12 +97,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'alya.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
-
 
 # DATABASES = {
 #     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
