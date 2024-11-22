d513c72c3de397e795918ec626e20da0f7e5b9be
Adsoog <iadso.ao@gmail.com>
2024-11-22 12:07:47 -0500

    fix some bugs

--- budget_commercial/urls.py
+++ budget_commercial/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import create_commercial_budget, detail_commercial_budget
+from .views import create_commercial_budget, detail_commercial_budget, index_budget_commercial
 
 urlpatterns = [
+    path('index_budget_commercial/', index_budget_commercial, name='index_budget_commercial'),
     path('create_commercial_budget/', create_commercial_budget, name='create_commercial_budget'),
     path('detail_commercial_budget/<int:pk>/', detail_commercial_budget, name='detail_commercial_budget'),
 ]
