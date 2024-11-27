008e1d90d48c9aed53f0d443079ad908ae50c3a4
Adsoog <iadso.ao@gmail.com>
2024-11-27 15:56:58 -0500

    añadido de tareas

--- follow_control_report/urls.py
+++ follow_control_report/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import valuations_view, daily_evaluation_cards
+from . import views
 
 urlpatterns = [
     path('valuations/<int:year>/<int:month>/', valuations_view, name='valuations'),
     path('daily-valuation/', daily_evaluation_cards, name='daily_evaluation_cards'),
-
+    path('etiquetar-tareas/', views.label_tasks, name='label_tasks'),
+   
 ]
