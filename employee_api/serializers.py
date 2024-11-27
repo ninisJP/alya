02aa0f6569addedbd2adb818aec0cbc56361abc8
mickfree <lydoived@gmail.com>
2024-11-27 16:08:42 -0500

    Add serializers cards, task & cardtaskorder

--- employee_api/serializers.py
+++ employee_api/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from follow_control_technician.models import TechnicianCard, TechnicianCardTask, TechnicianTask
+from follow_control_card.models import Task, CardTaskOrder, Card
+from accounting_order_sales.models import SalesOrder
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -33,4 +35,41 @@ class TechnicianCardSerializer(serializers.ModelSerializer):
         model = TechnicianCard
         fields = ['id', 'date', 'tasks']
         
+                
+# Cards
+class CardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Card
+        fields = ['id', 'name', 'description']
+
+class SalesOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SalesOrder
+        fields = [
+            'id', 'sapcode', 'project', 'detail', 'date', 
+            'total_sales_order', 'days', 'created_at', 'is_active'
+        ]
+        
+class TaskSerializer(serializers.ModelSerializer):
+    cards = CardSerializer(many=True, read_only=True)  
+    sale_order = SalesOrderSerializer(read_only=True)  
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Task
+        fields = [
+            'id', 'verb', 'object', 'sale_order', 'measurement', 
+            'task_time', 'user', 'cards'
+        ]
+
+    def validate_task_time(self, value):
+        if value and value < 0:
+            raise serializers.ValidationError("El tiempo de tarea debe ser positivo.")
+        return value
+    
+# Card Task Order
+class CardTaskOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CardTaskOrder
+        fields = ['card','task','order','state']
         
