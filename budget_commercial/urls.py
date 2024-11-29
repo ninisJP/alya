c78e269ffec92defe2455b4ac98b09c05af8fc64
mickfree <lydoived@gmail.com>
2024-11-29 15:13:39 -0500

    Add views for budget commercial
    - Add templates delete & detail - commecial_budget.html
    - Add partials forms & tables - budget_commercial.html
    - Add new urls for budget commerial
    - Add new views delete & detail

--- budget_commercial/urls.py
+++ budget_commercial/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import create_commercial_budget, detail_commercial_budget, index_budget_commercial
+from .views import create_commercial_budget, detail_commercial_budget, index_budget_commercial, delete_commercial_budget
 
 urlpatterns = [
     path('index_budget_commercial/', index_budget_commercial, name='index_budget_commercial'),
     path('create_commercial_budget/', create_commercial_budget, name='create_commercial_budget'),
     path('detail_commercial_budget/<int:pk>/', detail_commercial_budget, name='detail_commercial_budget'),
+    path('delete_commercial_budget/<int:pk>/', delete_commercial_budget, name='delete_commercial_budget'),
+
 ]
