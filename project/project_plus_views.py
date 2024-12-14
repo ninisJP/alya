3e69e8b6a1ca1a5d7d6da837f1566bf31c75d61d
Adsoog <iadso.ao@gmail.com>
2024-12-14 04:07:34 +0000

    almost done view newprojectplus

--- project/project_plus_views.py
+++ project/project_plus_views.py
@@ -1,44 +1,43 @@
-from django.db.models import Prefetch
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import ListView
-from accounting_order_sales.models import PurchaseOrderItem, SalesOrder, PurchaseOrder
-from logistic_inventory_input.models import InventoryInput
-from logistic_inventory_inputnewitem.models import InventoryInputNewItem
-from logistic_inventory_output.models import InventoryOutputItem
+from accounting_order_sales.models import PurchaseOrderItem, SalesOrder
 from logistic_requirements.models import RequirementOrderItem
-from .forms import ProjectForm
-from .models import Project
-from itertools import chain
 
 def project_order_plus(request, order_id):
-    # Obtener la SalesOrder
     sales_order = get_object_or_404(SalesOrder, id=order_id)
-    
-    # Obtener los ítems de SalesOrder
     sales_order_items = sales_order.items.all()
     
-    # Filtrar los RequirementOrders de SalesOrder donde purchase_order_created es False
-    requirement_orders = sales_order.requirement_orders.filter(purchase_order_created=False)
+    requirement_orders_items = RequirementOrderItem.objects.filter(
+        purchase_order_created=False,
+        requirement_order__sales_order=sales_order,
+        estado='L'  # Solo en estado 'Listo'
+    )
     
-    # Obtener los PurchaseOrders asociados
-    purchase_orders = sales_order.purchase_orders.all()
+    purchase_order_items = PurchaseOrderItem.objects.filter(
+        purchaseorder__salesorder=sales_order
+    )
     
-    # Obtener los ítems de RequirementOrder y PurchaseOrder
-    requirement_order_items = RequirementOrderItem.objects.filter(requirement_order__in=requirement_orders)
-    purchase_order_items = PurchaseOrderItem.objects.filter(purchaseorder__in=purchase_orders)
-    
-    # Combinamos los ítems de RequirementOrder y PurchaseOrder en una sola lista
-    combined_items = list(chain(requirement_order_items, purchase_order_items))
-    
-    # Enviar los datos al template
+    combined_items = list(requirement_orders_items) + list(purchase_order_items)
+
+    # Acceder al proyecto y al cliente
+    project = sales_order.project
+    client = project.client if project else None
+
+    # Creamos un diccionario para mapear la descripción (o SAP Code) al item
+    sales_order_item_dict = {item.description: item for item in sales_order_items}
+
+    # Ordenamos los ítems combinados de acuerdo al orden de los ítems en sales_order_items
+    # Basado en la descripción o el código SAP
+    combined_items.sort(key=lambda x: sales_order_item_dict.get(x.sales_order_item.description, None).id if x.sales_order_item.description in sales_order_item_dict else float('inf'))
+
     context = {
         'sales_order': sales_order,
         'sales_order_items': sales_order_items,
-        'purchase_orders': purchase_orders,
-        'requirement_orders': requirement_orders,
         'combined_items': combined_items,
+        'project': project,
+        'client': client,
     }
     
     return render(request, 'projectplus/project_order_plus.html', context)
+
 
 
