80130027218d371dbdc8e6de4ee0e5cb1fd0db8e
Michael Rómulo Luna Abad <81603830+mickfree@users.noreply.github.com>
2024-12-26 23:11:34 -0500

    Update settings.py

--- alya/settings.py
+++ alya/settings.py
@@ -99,11 +99,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'alya.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
