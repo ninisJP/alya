0ee59d8f251cd55ae9ddfefbc1d9ea19206a211c
mickfree <lydoived@gmail.com>
2024-12-30 09:24:42 -0500

    Add new colum TOTAL in requests App

--- alya/settings.py
+++ alya/settings.py
@@ -99,7 +99,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'alya.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
