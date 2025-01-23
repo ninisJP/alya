38340b3b9732bc3d3c1cdad7a282264b561cbaa6
Michael Rómulo Luna Abad <81603830+mickfree@users.noreply.github.com>
2025-01-23 15:27:06 -0500

    Update models.py

--- budget/models.py
+++ budget/models.py
@@ -61,6 +61,14 @@ class Budget(models.Model):
         self.budget_price = self.calculate_budget_price()
         self.save(update_fields=['budget_price'])
 
+    def remove_single_item_price(self, item):
+        """
+        Updates `budget_price` by subtracting the price of the deleted item
+        """
+        price_to_remove = item.total_price
+        self.budget_price -= price_to_remove
+        self.save(update_fields=['budget_price'])
+
     def save(self, *args, **kwargs):
         if not self.pk:
             # Guardar inicialmente para obtener un ID si es un nuevo presupuesto
